9225623f7038fffdccf830da1648565af4e236ee
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-05-19 08:11:54 -0700

    finishing with the MVP

--- server.js
+++ server.js
@@ -7,17 +7,48 @@ const app = express();
 
 //function to filter the query
 function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    // Note that we save the animalsArray as filteredResults here:
     let filteredResults = animalsArray;
-    if(query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    if (query.personalityTraits) {
+      // Save personalityTraits as a dedicated array.
+      // If personalityTraits is a string, place it into a new array and save.
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      // Loop through each trait in the personalityTraits array:
+      personalityTraitsArray.forEach(trait => {
+        // Check the trait against each animal in the filteredResults array.
+        // Remember, it is initially a copy of the animalsArray,
+        // but here we're updating it for each trait in the .forEach() loop.
+        // For each trait being targeted by the filter, the filteredResults
+        // array will then contain only the entries that contain the trait,
+        // so at the end we'll have an array of animals that have every one 
+        // of the traits when the .forEach() loop is finished.
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
     }
-    if(query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
-    if(query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
     }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return the filtered results:
     return filteredResults;
+  }
+
+//function to filter by id
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 //Creating a route for the front end can request data from
@@ -28,6 +59,16 @@ app.get('/api/animals', (req, res) => {
     }
     //res.send('Hello') //send() method from the res=response to send the string Hello back to our client
     res.json(results); 
+});
+
+//Creating another route with param
+app.get('/api/animals/:id', (req, res) => { //param route must come after the firt GET method-> above
+    const result = findById(req.params.id, animals);
+    if(result){
+        res.json(result);
+    }else{
+        res.send(404);
+    }    
 });
 
 //to make the server listen to requests
