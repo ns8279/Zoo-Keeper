fe7ec1aaa74c4de634b4c95a7110b1f34164f682
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-05-18 19:07:58 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ const {animals} = require('./data/animals');
 //To instantiate the server
 const app = express();
 
+//function to filter the query
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if(query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if(query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if(query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
 //Creating a route for the front end can request data from
-app.get('./api/animals', (req, res) => {
-    res.send('Hello'); //send() method from the res=response to send the string Hello back to our client
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results); //passing the filterByQuery function with the query parameter = req.query and animalsArray = animals.json(results)
+    }
+    //res.send('Hello') //send() method from the res=response to send the string Hello back to our client
+    res.json(results); 
 });
 
 //to make the server listen to requests
